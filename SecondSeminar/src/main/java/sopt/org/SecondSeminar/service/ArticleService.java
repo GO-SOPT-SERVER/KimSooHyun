5e5658496efd5482656043794e501487acbf48a0
soohyun <sss4920@naver.com>
2023-04-17 14:42:25 +0900

    카멜 케이스 및 스트림사용해서 search메소드 수정

--- SecondSeminar/src/main/java/sopt/org/SecondSeminar/service/ArticleService.java
+++ SecondSeminar/src/main/java/sopt/org/SecondSeminar/service/ArticleService.java
@@ -3,13 +3,15 @@ package sopt.org.SecondSeminar.service;
 import org.springframework.stereotype.Service;
 import sopt.org.SecondSeminar.controller.article.dto.request.RegisterRequestDto;
 import sopt.org.SecondSeminar.domain.article.Article;
+import java.util.Optional;
+
 import static sopt.org.SecondSeminar.SecondSeminarApplication.articleList;
 
 @Service
 public class ArticleService {
     public Long registerArticle(RegisterRequestDto request) {
 
-        // 받아온 request 데이터를 토대로 실제 User 객체 생성
+        // 받아온 request 데이터를 토대로 실제 Article 객체 생성
         Article newArticle = new Article(
                 request.getTitle(),
                 request.getText()
@@ -19,31 +21,23 @@ public class ArticleService {
         articleList.add(newArticle);
         newArticle.setId((long) articleList.size());
 
-        // 저장한 유저 아이디 값 반환
+        // 저장한 게시물 아이디 값 반환
         return newArticle.getId();
     }
     public String searchByArticleTitle(String title){
-        for (Article wantArticle: articleList){
-            if (wantArticle.getTitle().equals(title)){
-                Long num = wantArticle.getId();
-                String temp_title = wantArticle.getTitle();
-                String temp_text = wantArticle.getText();
-                return "id="+num+"\n title="+temp_title+"\n text="+temp_text;
+        Optional<Article> article = articleList.stream()    //게시물 리스트를 stream으로 만들고 제목 같은 게시물 중 첫번째 값 저장
+                .filter(wantPost -> wantPost.getTitle().equals(title))
+                .findFirst();
+                return article.map(wantPost->"id="+wantPost.getId()+"\ntitle="+wantPost.getTitle()+"\ntext="+wantPost.getText())  //map으로 스트림의 요소에 저장된 값 중 원하는 필드 뽑아내기
+                        .orElse("게시물이 존재하지 않습니다."); //조건에 맞는게 없으면 이 문자열 리턴
             }
-        }
-        return "게시물이 존재하지 않습니다.";
 
+    public String searchByArticleId(Long articleId){
+        Optional<Article> article = articleList.stream()    //게시물 리스트를 stream으로 만들고 아이디 같은 게시물 하나 뽑음.
+                .filter(wantPost -> wantPost.getId().equals(articleId))
+                .findFirst();       //근데 어차피 id값은 pk라 하나일텐데 그냥 findFirst 박아야되나?
+        return article.map(wantPost->"id="+wantPost.getId()+"\ntitle="+wantPost.getTitle()+"\ntext="+wantPost.getText())  //map으로 스트림의 요소에 저장된 값 중 원하는 필드 뽑아내기
+                .orElse("게시물이 존재하지 않습니다."); //조건에 맞는게 없으면 이 문자열 리턴
     }
-    public String searchByArticleId(Long article_id){
-        for (Article wantArticle: articleList){
-            if (wantArticle.getId()==article_id){
-                Long num = wantArticle.getId();
-                String temp_title = wantArticle.getTitle();
-                String temp_text = wantArticle.getText();
-                return "id="+num+"\n title="+temp_title+"\n text="+temp_text;
-            }
-        }
-        return "게시물을 찾을 수 없습니다.";
 
-    }
 }
